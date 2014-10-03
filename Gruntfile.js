8808bb8e6f66ec1db20e7b4e248513d5e0f81ef6
Ash Edwards <info@ashedwardsdesign.co.uk>
2014-10-04 00:53:41 +0100

    Grunt anyone?

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,16 +5,18 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		// Concat will concantenate (join) any files specified - Javascript in this case
+		
 		concat: {
-			// 2. Configuration for concatinating files goes here.
 			dist: {
-
 				files: {
 					'dist/scripts/ie.js': ['scripts/html5shiv.js', 'scripts/respond.min.js'],
 					'dist/scripts/litework.js': ['scripts/jquery-1.11.1.min.js', 'scripts/jquery.slicknav.min.js']
 				}
 			}
 		},
+
+		// Uglify will minify any Javascript specified - beforehand be sure to concat where possible!
 
 		uglify: {
 			my_target: {
@@ -26,6 +28,8 @@ module.exports = function (grunt) {
 			}
 		},
 
+		// Image min will compress all images in the "img/" source folder with PNG, JPG or GIF extensions
+
 		imagemin: {
 			dynamic: {
 				files: [{
@@ -36,6 +40,8 @@ module.exports = function (grunt) {
         }]
 			}
 		},
+
+		// HTML Min for minifying all HTML in the root - set to remove both comments and whitespace
 
 		htmlmin: {
 			dist: {
@@ -52,15 +58,21 @@ module.exports = function (grunt) {
 			}
 		}
 
+		// Put SASS here
+		
+		// Put Autoprefixer here
+		
+		// WATCH stuff goes here
+
 	});
 
-	// 3. Where we tell Grunt we plan to use this plug-in.
+	// List of used plugins
 	grunt.loadNpmTasks("grunt-contrib-concat");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-imagemin");
 	grunt.loadNpmTasks("grunt-contrib-htmlmin");
 
-	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-	grunt.registerTask('default', ["concat", "uglify", "imagemin", "htmlmin", ]);
+	// Here we tell Grunt what to do when we type 'grunt' into the CLI
+	grunt.registerTask('default', ["concat", "uglify", "imagemin", "htmlmin"]);
 
 };
