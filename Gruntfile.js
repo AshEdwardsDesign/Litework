087e8f9e67a5dfdcd57b58544ae39c9e9f9c64a4
Ash Edwards <info@ashedwardsdesign.co.uk>
2014-10-03 09:42:59 +0100

    Further grunt stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,72 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // 1. All configuration goes here 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	// 1. All configuration goes here 
 
-        concat: {
-            // 2. Configuration for concatinating files goes here.
-             dist: {
-        src: [
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		concat: {
+			// 2. Configuration for concatinating files goes here.
+			dist: {
+				src: [
             'scripts/html5shiv.js',
             'scripts/respond.min.js'
         ],
-        dest: 'dist/scripts/ie.js',
-    }
-        }
+				dest: 'dist/scripts/ie.js',
+			},
 
-    });
+			dist: {
+				src: [
+            'scripts/jquery-1.11.1.min.js',
+            'scripts/jquery.slicknav.min.js'
+        ],
+				dest: 'dist/scripts/litework.js',
+			}
+		},
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
+		uglify: {
+			build: {
+				src: 'dist/scripts/ie.js',
+				dest: 'dist/scripts/ie.min.js',
+			},
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+			build: {
+				src: 'dist/scripts/litework.js',
+				dest: 'dist/scripts/litework.min.js',
+			}
+		},
+
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'img/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'dist/img/',
+        }]
+			}
+		},
+
+		htmlmin: {
+			dev: {
+				files: [{
+					expand: true,
+					cwd: '',
+					src: '*.html',
+					dest: 'dist/'
+            }]
+			}
+		}
+
+	});
+
+	// 3. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-imagemin");
+	grunt.loadNpmTasks("grunt-contrib-htmlmin");
+
+	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('default', ["concat", "uglify", "imagemin", "htmlmin", ]);
+
+};
