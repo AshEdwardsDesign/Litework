a489d85c3f24e2d3e23e126835310fef3868e86b
Ash Edwards <info@ashedwardsdesign.co.uk>
2015-01-26 13:21:00 +0000

    further work

---
+++ cfg-form-3/inc/form.config.php
@@ -0,0 +1,41 @@
+<?php
+$cfg['debug'] = false;
+
+// This is the email address where you will receive the notification message
+$cfg['email_address'] = 'info@nonsurgicalbodyclinic.co.uk';
+$cfg['email_from'] = 'Non Surgical Body Clinic';
+
+// The recipients in CC and BCC will receive a copy of the data collected in the form
+// Use commas to separate mutiple e-mail addresses (no spaces allowed)
+// Example: youraddress1@yourdomain.com,youraddress2@yourdomain.com
+$cfg['email_address_cc'] = '';
+$cfg['email_address_bcc'] = '';
+$cfg['emailsendingmethod'] = 'php';
+$cfg['smtp_host'] = '';
+$cfg['smtp_port'] = '';
+$cfg['smtp_encryption'] = '';
+$cfg['smtp_username'] = '';
+$cfg['smtp_password'] = '';
+$cfg['timezone'] = 'UTC';
+$cfg['form_name'] = 'Non Surgical Body Clinic';
+$cfg['form_validationmessage'] = 'Thank you, your message has been sent to us.<br />We will get back to you as soon as possible.';
+$cfg['form_errormessage_captcha'] = 'Value does not match';
+$cfg['form_errormessage_emptyfield'] = 'This field cannot be left blank';
+$cfg['form_errormessage_invalidemailaddress'] = 'Invalid email address';
+$cfg['form_errormessage_invalidurl'] = '';
+$cfg['form_redirecturl'] = '';
+$cfg['adminnotification_subject'] = 'New message sent from your website';
+$cfg['adminnotification_hideemptyvalues'] = false;
+$cfg['adminnotification_hideformurl'] = true;
+$cfg['usernotification_inputid'] = 'cfg-element-3-1';
+$cfg['usernotification_activate'] = true;
+$cfg['usernotification_insertformdata'] = true;
+$cfg['usernotification_format'] = 'plaintext';
+$cfg['usernotification_subject'] = 'Thank you for your message';
+$cfg['usernotification_message'] = 'Thank you for contacting us.
+We will answer you as soon as possible.';
+$cfg['usernotification_hideemptyvalues'] = true;
+$cfg['formvalidation_required'][] = 'cfg-element-3-6';
+$cfg['formvalidation_required'][] = 'cfg-element-3-4';
+$cfg['formvalidation_email'][] = 'cfg-element-3-1';
+?>
