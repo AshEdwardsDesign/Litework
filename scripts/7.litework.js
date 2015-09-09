b92eea94af3e69f04db79c890731c63cb638feec
Ash Edwards <info@ashedwardsdesign.co.uk>
2015-09-09 22:16:54 +0100

    first draft

--- scripts/7.litework.js
+++ scripts/7.litework.js
@@ -4,19 +4,7 @@ $(function() {
   $('#menu').slicknav();
 });
 
-// ScrollReveal
-(function($) {
 
-  'use strict';
-
-  window.sr = new scrollReveal({
-    reset: false,
-    mobile: false
-  });
-
-})();
-
-// Hero Centering
 $(document).ready(function() {
   $('#hero-centered').flexVerticalCenter({
 
@@ -24,51 +12,9 @@ $(document).ready(function() {
   });
 });
 
-// Call Isotope
-$('.grid').isotope({
+$(document).ready(function() {
+  $('#navstripcentered').flexVerticalCenter({
 
-  // bind category button click
-  $('.category-buttons').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[filterValue] || filterValue;
-    $grid.isotope({
-      filter: filterValue
-    });
+    parentSelector: '#navstrip'
   });
-
-  /*
-
-          // bind price button click
-          $('.price-buttons').on('click', 'button', function() {
-            var filterValue = $(this).attr('data-filter');
-            // use filterFn if matches value
-            filterValue = filterFns[filterValue] || filterValue;
-            $grid.isotope({
-              filter: filterValue
-            });
-          });
-
-  */
-
-  // change is-checked class on buttons
-  $('.button-group').each(function(i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function() {
-      $buttonGroup.find('.active').removeClass('active');
-      $(this).addClass('active');
-    });
-  });
-
-  // options
-  itemSelector: '.grid-item',
-    layoutMode: 'fitRows',
-    percentPosition: true,
-    masonry: {
-      // use outer width of grid-sizer for columnWidth
-      columnWidth: '.grid-sizer',
-      gutter: 10
-    }
-
-
 });
