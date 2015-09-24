ef7fe6800737bd4fb611bd622bf171a831c2a55c
Ash Edwards <info@ashedwardsdesign.co.uk>
2015-09-24 17:46:58 +0100

    commented out isotope for now

--- scripts/7.litework.js
+++ scripts/7.litework.js
@@ -18,57 +18,57 @@ $(function() {
 
 // Hero Centering
 $(document).ready(function() {
-  $('#hero-centered').flexVerticalCenter({
+$('#hero-centered').flexVerticalCenter({
 
-    parentSelector: '#hero'
-  });
+  parentSelector: '#hero'
+});
 });
 
-// Call Isotope
-$('.grid').isotope({
-
-  // bind category button click
-  $('.category-buttons').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[filterValue] || filterValue;
-    $grid.isotope({
-      filter: filterValue
-    });
-  });
-
-  /*
-
-          // bind price button click
-          $('.price-buttons').on('click', 'button', function() {
-            var filterValue = $(this).attr('data-filter');
-            // use filterFn if matches value
-            filterValue = filterFns[filterValue] || filterValue;
-            $grid.isotope({
-              filter: filterValue
-            });
-          });
-
-  */
-
-  // change is-checked class on buttons
-  $('.button-group').each(function(i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function() {
-      $buttonGroup.find('.active').removeClass('active');
-      $(this).addClass('active');
-    });
-  });
-
-  // options
-  itemSelector: '.grid-item',
-    layoutMode: 'fitRows',
-    percentPosition: true,
-    masonry: {
-      // use outer width of grid-sizer for columnWidth
-      columnWidth: '.grid-sizer',
-      gutter: 10
-    }
+// // Call Isotope
+// $('.grid').isotope({
+//
+//   // bind category button click
+//   $('.category-buttons').on('click', 'button', function() {
+//     var filterValue = $(this).attr('data-filter');
+//     // use filterFn if matches value
+//     filterValue = filterFns[filterValue] || filterValue;
+//     $grid.isotope({
+//       filter: filterValue
+//     });
+//   });
+//
+//   /*
+//
+//           // bind price button click
+//           $('.price-buttons').on('click', 'button', function() {
+//             var filterValue = $(this).attr('data-filter');
+//             // use filterFn if matches value
+//             filterValue = filterFns[filterValue] || filterValue;
+//             $grid.isotope({
+//               filter: filterValue
+//             });
+//           });
+//
+//   */
+//
+//   // change is-checked class on buttons
+//   $('.button-group').each(function(i, buttonGroup) {
+//     var $buttonGroup = $(buttonGroup);
+//     $buttonGroup.on('click', 'button', function() {
+//       $buttonGroup.find('.active').removeClass('active');
+//       $(this).addClass('active');
+//     });
+//   });
+//
+//   // options
+//   itemSelector: '.grid-item',
+//     layoutMode: 'fitRows',
+//     percentPosition: true,
+//     masonry: {
+//       // use outer width of grid-sizer for columnWidth
+//       columnWidth: '.grid-sizer',
+//       gutter: 10
+//     }
 
 
 });
