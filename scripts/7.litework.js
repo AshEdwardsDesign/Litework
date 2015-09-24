fe3977971f3f260c0bb0728f277edbcab5ce14eb
Ash Edwards <info@ashedwardsdesign.co.uk>
2015-09-24 15:23:57 +0100

    some amendments

--- scripts/7.litework.js
+++ scripts/7.litework.js
@@ -4,14 +4,6 @@ $(function() {
   $('#menu').slicknav();
 });
 
-
-$(document).ready(function() {
-  $('#hero-centered').flexVerticalCenter({
-
-    parentSelector: '#hero'
-  });
-});
-
 $(document).ready(function() {
   $('#navstripcentered').flexVerticalCenter({
 
