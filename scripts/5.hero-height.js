520f91658bf1e79ca50fdc9b9271b408d6fa7e13
Ash Edwards <info@ashedwardsdesign.co.uk>
2016-02-25 19:39:54 +0000

    more work

---
+++ scripts/5.hero-height.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    $('#homepage-hero').css('min-height', windowHeight);
+  };
+  setHeight();
+
+  $(window).resize(function() {
+    setHeight();
+  });
+});
---
+++ scripts/6.litework.js
@@ -0,0 +1,7 @@
+// Hero Centering
+$(document).ready(function() {
+  $('#hero-centered').flexVerticalCenter({
+
+    parentSelector: '#homepage-hero'
+  });
+});
