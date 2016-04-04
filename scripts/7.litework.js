998e3a2e77204ce6992684dee3c168796bed09ce
Ash Edwards <info@ashedwardsdesign.co.uk>
2016-04-04 11:34:05 +0100

    more work

---
+++ scripts/7.litework.js
@@ -0,0 +1,19 @@
+// ScrollReveal
+(function($) {
+
+  'use strict';
+
+  window.sr = new scrollReveal({
+    reset: false,
+    mobile: false
+  });
+
+})();
+
+// Hero Centering
+$(document).ready(function() {
+  $('#hero-centered').flexVerticalCenter({
+
+    parentSelector: '#hero'
+  });
+});
