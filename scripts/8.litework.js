fa25be66d5a8a6204110bbd7165d42875cb257ce
Ash Edwards <info@ashedwardsdesign.co.uk>
2016-06-01 15:14:07 +0100

    add addthis bar

--- scripts/8.litework.js
+++ scripts/8.litework.js
@@ -1,3 +1,15 @@
 $(function() {
     $('#menu').slicknav();
 });
+
+// Load Addthis share bar after page load onload();
+function downloadJSAtOnload() {
+    var element = document.createElement("script");
+    element.src = "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-574ed955612875d5";
+    document.body.appendChild(element);
+}
+if (window.addEventListener)
+    window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+    window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
