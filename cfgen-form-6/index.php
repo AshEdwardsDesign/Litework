93546ae2ceb32834160a31443ac7034c2a1a664e
Ash Edwards <info@ashedwardsdesign.co.uk>
2016-12-29 15:48:47 +0000

    initial launch

---
+++ cfgen-form-6/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+
+<meta charset="utf-8">
+
+<title>Inch Go Lipo</title>
+
+
+<!-- Form Start -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script src="cfgen-form-6/js/form.js"></script>
+<link href="cfgen-form-6/css/form.css" rel="stylesheet" type="text/css">
+
+<!-- Form End -->
+
+
+</head>
+
+<body>
+
+<div class="cfgen-form-container" id="cfgen-form-6">
+
+<div class="cfgen-form-content">
+
+<div class="cfgen-e-c">
+
+	<label class="cfgen-label" id="cfgen-element-6-6-label" ><span class="cfgen-label-value">First Name</span><span class="cfgen-required">*</span></label>
+
+	<div class="cfgen-e-set" id="cfgen-element-6-6-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-6-6-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-6-6-input-c">
+				<input type="text" class="cfgen-type-text cfgen-form-value " name="cfgen-element-6-6" id="cfgen-element-6-6" placeholder="First Name">
+			</div>
+		</div>
+	</div>
+
+	<div class="cfgen-clear"></div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<label class="cfgen-label" id="cfgen-element-6-7-label" ><span class="cfgen-label-value">Last Name</span><span class="cfgen-required">*</span></label>
+
+	<div class="cfgen-e-set" id="cfgen-element-6-7-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-6-7-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-6-7-input-c">
+				<input type="text" class="cfgen-type-text cfgen-form-value " name="cfgen-element-6-7" id="cfgen-element-6-7" placeholder="Last Name">
+			</div>
+		</div>
+	</div>
+
+	<div class="cfgen-clear"></div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<label class="cfgen-label" id="cfgen-element-6-3-label" ><span class="cfgen-label-value">Email address</span><span class="cfgen-required">*</span></label>
+
+	<div class="cfgen-e-set" id="cfgen-element-6-3-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-6-3-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-6-3-input-c">
+				<input type="text" class="cfgen-type-email cfgen-form-value " name="cfgen-element-6-3" id="cfgen-element-6-3" placeholder="Email address">
+			</div>
+		</div>
+	</div>
+
+	<div class="cfgen-clear"></div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<label class="cfgen-label" id="cfgen-element-6-8-label" ><span class="cfgen-label-value">Phone</span><span class="cfgen-required">*</span></label>
+
+	<div class="cfgen-e-set" id="cfgen-element-6-8-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-6-8-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-6-8-input-c">
+				<input type="text" class="cfgen-type-text cfgen-form-value " name="cfgen-element-6-8" id="cfgen-element-6-8" placeholder="Phone">
+			</div>
+		</div>
+	</div>
+
+	<div class="cfgen-clear"></div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<label class="cfgen-label" id="cfgen-element-6-4-label" ><span class="cfgen-label-value">Your Message</span><span class="cfgen-required">*</span></label>
+
+	<div class="cfgen-e-set" id="cfgen-element-6-4-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-6-4-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-6-4-input-c">
+				<textarea class="cfgen-type-textarea cfgen-form-value " name="cfgen-element-6-4" id="cfgen-element-6-4" placeholder="Your Message"></textarea>
+			</div>
+		</div>
+	</div>
+
+	<div class="cfgen-clear"></div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<label class="cfgen-label" id="cfgen-element-6-11-label" ><span class="cfgen-label-value">Type captcha here</span></label>
+
+	<div class="cfgen-e-set" id="cfgen-element-6-11-set-c">
+		<div class="cfgen-captcha-c">
+			<img src="cfgen-form-6/inc/captcha.php" class="cfgen-captcha-img" alt="" ><img src="cfgen-form-6/img/captcha-refresh.png" class="cfgen-captcha-refresh" alt="">
+		</div>
+		<div class="cfgen-input-group" id="cfgen-element-6-11-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-6-11-input-c">
+				<input type="text" name="cfgen-element-6-11" id="cfgen-element-6-11" class="cfgen-captcha-input" placeholder="Type captcha here">
+			</div>
+		</div>
+	</div>
+
+	<div class="cfgen-clear"></div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<div class="cfgen-e-set" id="cfgen-element-6-5-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-6-5-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-6-5-input-c">
+				<input type="submit" class="cfgen-submit" name="cfgen-element-6-5" id="cfgen-element-6-5" value="Send">
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+<div class="cfgen-loading"></div>
+
+</div><!-- cfgen-form-content -->
+
+</div><!-- cfgen-form-container -->
+
+</body>
+
+
+</html>
